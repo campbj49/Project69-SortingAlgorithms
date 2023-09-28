9b3d4537a162f77d2894641d3ae210eae45e7e9a
John Campbell <campbj49@gmail.com>
2023-09-28 14:53:28 -0400

    finish bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,10 @@
-function bubbleSort() {}
+/**Accepts an array of numbers and returns it sorted using the bubble algorithm */
+
+function bubbleSort(array) {
+    for(i=array.length; i>0; i--)
+        for(j=0; j<i; j++)
+            if(array[j]>array[j+1]) [array[j], array[j+1]] = [array[j+1], array[j]]
+    return array;
+}
 
 module.exports = bubbleSort;
