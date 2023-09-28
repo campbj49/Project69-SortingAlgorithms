a7bae9b9c19640ec413dc1fc4ca17491a41d0411
John Campbell <campbj49@gmail.com>
2023-09-28 15:13:08 -0400

    insertion sort works

--- insertion.js
+++ insertion.js
@@ -1,3 +1,21 @@
-function insertionSort() {}
+/**Impementation of the insertion sort algorithm */
+function insertionSort(array) {
+    for(i=1; i<array.length; i++){
+        //find incorrectly placed elements
+        if(array[i]< array[i-1]){
+            //place them correctly in the sorted part of the array
+            for(j=i-1; j>=0; j--){
+                if(j===0 || array[i]>array[j-1]){
+                    //splice value into array
+                    array = [...array.slice(0,j),array[i],...array.slice(j,i), ...array.slice(i+1, array.length)]
+                    j=0;
+                }
+            }
+
+        }
+    }
+
+    return array;
+}
 
 module.exports = insertionSort;
