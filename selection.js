22ee1cf69da993684b84621dcad2874049c41a0d
John Campbell <campbj49@gmail.com>
2023-09-28 17:32:06 -0400

    selection sort passing tests

--- selection.js
+++ selection.js
@@ -1,3 +1,17 @@
-function selectionSort() {}
+/**Sorts numbers with the selection sort algorithm */
+function selectionSort(array) {
+    //itereate through the whole array
+    for(i=0; i<array.length; i++){
+        let minDex = i;
+        //search the rest of the array for a smaller val
+        for(j=i; j<array.length; j++){
+            if(array[j]<array[minDex])
+                minDex = j;
+        }
+        if(minDex != i) [array[i], array[minDex]] = [array[minDex], array[i]]
+    }
+
+    return array;
+}
 
 module.exports = selectionSort;
